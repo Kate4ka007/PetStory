896628d3a9c79d285e0ba5fc6439b497798cb57e
Kate4ka007 <shlemena@mail.ru>
2022-12-09 14:01:15 +0300

    feat: add modules

--- Pet-Story/src/app/app-routing.module.ts
+++ Pet-Story/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'start-page',
+    loadChildren: () =>
+      import('./start-page/start-page.module').then((m) => m.StartPageModule),
+  },
+  {
+    path: 'map',
+    loadChildren: () => import('./map/map.module').then((m) => m.MapModule),
+  },
+  {
+    path: 'animals',
+    loadChildren: () =>
+      import('./animals/animals.module').then((m) => m.AnimalsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
