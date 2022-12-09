4fe72d2fa80a68d78296d10c01a460f27af75f32
Kate4ka007 <shlemena@mail.ru>
2022-12-09 15:12:46 +0300

    feat: add components

---
+++ Pet-Story/src/app/core/components/testimonails/testimonails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonails',
+  templateUrl: './testimonails.component.html',
+  styleUrls: ['./testimonails.component.scss']
+})
+export class TestimonailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Pet-Story/src/app/core/core.module.ts
+++ Pet-Story/src/app/core/core.module.ts
@@ -3,11 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { CoreComponent } from './core.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { MainImageComponent } from './components/main-image/main-image.component';
+import { BackstageComponent } from './components/backstage/backstage.component';
+import { PickComponent } from './components/pick/pick.component';
+import { TestimonailsComponent } from './components/testimonails/testimonails.component';
 
 
 @NgModule({
   declarations: [
-    CoreComponent
+    CoreComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    MainImageComponent,
+    BackstageComponent,
+    PickComponent,
+    TestimonailsComponent
   ],
   imports: [
     CommonModule,
