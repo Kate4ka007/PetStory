5e01b4a99b7ecef7c295a15847466ba43693921b
Kate4ka007 <shlemena@mail.ru>
2022-12-14 23:36:45 +0300

    feat: add slider

--- Pet-Story/src/app/start-page/components/main-image/main-image.component.ts
+++ Pet-Story/src/app/start-page/components/main-image/main-image.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { slider } from 'src/assets/data/slider';
+
+interface Slider {
+  name: string;
+  src: string;
+  from: string;
+  food: string;
+}
 
 @Component({
   selector: 'app-main-image',
@@ -6,10 +14,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-image.component.scss']
 })
 export class MainImageComponent implements OnInit {
+  @ViewChild('wrapper') wrapper!: ElementRef;
+
+  animals: Slider[] = slider;
+
+  marginLeft = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  moveRight() {
+    if (this.marginLeft < 0) {
+      this.marginLeft += 1188;
+      this.wrapper.nativeElement.style.marginLeft = `${this.marginLeft}px`;
+
+    }
+
+  }
+  moveLeft() {
+    if (this.marginLeft > -3500)
+      this.marginLeft -= 1188;
+    this.wrapper.nativeElement.style.marginLeft = `${this.marginLeft}px`;
   }
 
 }
