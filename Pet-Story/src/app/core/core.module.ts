8c4f68e845f22fa466aead9f42cff7ea080e63ac
Kate4ka007 <shlemena@mail.ru>
2022-12-27 19:26:10 +0300

    fix: routing

--- Pet-Story/src/app/core/core.module.ts
+++ Pet-Story/src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { ButtonComponent } from './components/button/button.component';
+import { CoreRoutingModule } from './core-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ButtonComponent } from './components/button/button.component';
   ],
   imports: [
     CommonModule,
+    CoreRoutingModule,
   ],
-  exports: [HeaderComponent, FooterComponent, ButtonComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent, ButtonComponent]
 })
 export class CoreModule { }
