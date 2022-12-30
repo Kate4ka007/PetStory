833f7c2505422476232cb44a4a2b7149a6e5eb6e
Kate4ka007 <shlemena@mail.ru>
2022-12-30 20:07:33 +0300

    fix: update map

--- Pet-Story/src/app/map/map.component.ts
+++ Pet-Story/src/app/map/map.component.ts
@@ -1,3 +1,4 @@
+import { AnimationKeyframesSequenceMetadata } from '@angular/animations';
 import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
 import * as L from 'leaflet';
 
@@ -17,21 +18,58 @@ export class MapComponent implements AfterViewInit {
     this.loadMap();
   }
 
+  /*  private loadMap(): void {
+     this.map = L.map('map', {
+       crs: L.CRS.Simple,
+       minZoom: -1.6,
+       maxZoom: 2,
+       id: 'mapbox',
+     }).setView([0, 0], -1.6);
+
+
+     const bounds = [[-1000, -2000], [1000, 2000]];
+     L.imageOverlay('assets/images/map.svg', bounds).addTo(this.map);
+
+     const icon = L.icon({
+       iconUrl: 'assets/images/marker.svg',
+       popupAnchor: [-10, -10],
+     });
+
+     const pandaIcon = L.icon({
+       iconUrl: 'assets/images/panda-marker.svg',
+       popupAnchor: [-10, -10],
+     });
+
+     const buttonRemove =
+       '<button type="button" class="remove">delete marker</button>';
+
+
+     const marker = L.marker([2080, 950], { icon }).addTo(this.map).bindPopup(buttonRemove);
+     const marker2 = L.marker([2180, 850], { icon }).addTo(this.map).bindPopup(buttonRemove);
+     const panda = L.marker([500, 1000], { icon: pandaIcon }).addTo(this.map).bindPopup(buttonRemove);
+
+     const marker3 = L.marker([0, 0], { icon }).addTo(this.map).bindPopup(buttonRemove);
+
+
+
+   } */
+
   private loadMap(): void {
     this.map = L.map('map', {
-      crs: L.CRS.Simple,
-      minZoom: -1.6,
-      maxZoom: 2,
-      id: 'mapbox',
-    }).setView([0, 0], -1.6);
+      minZoom: 1,
+      maxZoom: 6,
+      zoomControl: false
+    }).setView([0, 0], 1);
 
-
-    const bounds = [[-1000, -2000], [1000, 2000]];
+    L.control.zoom({
+      position: 'bottomright'
+    }).addTo(this.map);
+    const bounds = [[-940, -621], [940, 621]];
     L.imageOverlay('assets/images/map.svg', bounds).addTo(this.map);
 
     const icon = L.icon({
       iconUrl: 'assets/images/marker.svg',
-      popupAnchor: [0, 0],
+      popupAnchor: [10, 10],
     });
 
     const pandaIcon = L.icon({
@@ -39,14 +77,15 @@ export class MapComponent implements AfterViewInit {
       popupAnchor: [10, 10],
     });
 
+    const buttonRemove =
+      '<button type="button" class="remove">delete marker</button>';
 
-    const marker = L.marker([2080, 950], { icon }).bindPopup('Angular Leaflet');
-    marker.addTo(this.map);
+    const marker = L.marker([0, 0], { icon }).addTo(this.map).bindPopup('buttonRemove');
+    const marker2 = L.marker([100, 100], { icon }).addTo(this.map).bindPopup('buttonRemove');
+    const panda = L.marker([-100, -100], { icon: pandaIcon }).addTo(this.map).bindPopup('buttonRemove');
 
-    const marker2 = L.marker([2180, 850], { icon }).bindPopup('Angular Leaflet2');
-    marker2.addTo(this.map);
 
-    const panda = L.marker([2000, 950], { icon: pandaIcon }).bindPopup('Angular Leaflet');
-    panda.addTo(this.map);
+
+    const marker3 = L.marker([200, 200], { icon }).addTo(this.map).bindPopup('buttonRemove');
   }
 }
