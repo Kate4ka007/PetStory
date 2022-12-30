1a0d0fc127c9cb57ec0d98ac3dd4298cbf4b0154
Kate4ka007 <shlemena@mail.ru>
2022-12-30 16:03:22 +0300

    feat: add leaflet and map

--- Pet-Story/src/app/map/map.component.ts
+++ Pet-Story/src/app/map/map.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
+  map: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  public ngAfterViewInit(): void {
+    this.loadMap();
+  }
+
+  private loadMap(): void {
+    this.map = L.map('map', {
+      crs: L.CRS.Simple,
+      minZoom: -1.6,
+      maxZoom: 2,
+      id: 'mapbox',
+    }).setView([0, 0], -1.6);
+
+
+    const bounds = [[-1000, -2000], [1000, 2000]];
+    L.imageOverlay('assets/images/map.svg', bounds).addTo(this.map);
+
+    const icon = L.icon({
+      iconUrl: 'assets/images/marker.svg',
+      popupAnchor: [0, 0],
+    });
+
+    const pandaIcon = L.icon({
+      iconUrl: 'assets/images/panda-marker.svg',
+      popupAnchor: [10, 10],
+    });
+
+
+    const marker = L.marker([2080, 950], { icon }).bindPopup('Angular Leaflet');
+    marker.addTo(this.map);
+
+    const marker2 = L.marker([2180, 850], { icon }).bindPopup('Angular Leaflet2');
+    marker2.addTo(this.map);
+
+    const panda = L.marker([2000, 950], { icon: pandaIcon }).bindPopup('Angular Leaflet');
+    panda.addTo(this.map);
+  }
 }
