90ad6ba56fbf89773da8eba337e709213bddc522
Kate4ka007 <shlemena@mail.ru>
2022-12-31 16:10:28 +0300

    feat: add markers for pandas

--- Pet-Story/src/app/map/map.component.ts
+++ Pet-Story/src/app/map/map.component.ts
@@ -1,4 +1,3 @@
-import { AnimationKeyframesSequenceMetadata } from '@angular/animations';
 import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
 import * as L from 'leaflet';
 
@@ -18,74 +17,69 @@ export class MapComponent implements AfterViewInit {
     this.loadMap();
   }
 
-  /*  private loadMap(): void {
-     this.map = L.map('map', {
-       crs: L.CRS.Simple,
-       minZoom: -1.6,
-       maxZoom: 2,
-       id: 'mapbox',
-     }).setView([0, 0], -1.6);
-
-
-     const bounds = [[-1000, -2000], [1000, 2000]];
-     L.imageOverlay('assets/images/map.svg', bounds).addTo(this.map);
-
-     const icon = L.icon({
-       iconUrl: 'assets/images/marker.svg',
-       popupAnchor: [-10, -10],
-     });
-
-     const pandaIcon = L.icon({
-       iconUrl: 'assets/images/panda-marker.svg',
-       popupAnchor: [-10, -10],
-     });
-
-     const buttonRemove =
-       '<button type="button" class="remove">delete marker</button>';
-
-
-     const marker = L.marker([2080, 950], { icon }).addTo(this.map).bindPopup(buttonRemove);
-     const marker2 = L.marker([2180, 850], { icon }).addTo(this.map).bindPopup(buttonRemove);
-     const panda = L.marker([500, 1000], { icon: pandaIcon }).addTo(this.map).bindPopup(buttonRemove);
-
-     const marker3 = L.marker([0, 0], { icon }).addTo(this.map).bindPopup(buttonRemove);
-
-
-
-   } */
-
   private loadMap(): void {
     this.map = L.map('map', {
       minZoom: 1,
-      maxZoom: 6,
+      maxZoom: 4,
       zoomControl: false
-    }).setView([0, 0], 1);
+    }).setView([0, 0], 1.5);
 
     L.control.zoom({
       position: 'bottomright'
     }).addTo(this.map);
-    const bounds = [[-940, -621], [940, 621]];
-    L.imageOverlay('assets/images/map.svg', bounds).addTo(this.map);
 
-    const icon = L.icon({
-      iconUrl: 'assets/images/marker.svg',
-      popupAnchor: [10, 10],
-    });
+    const bounds = [[-1085, -717], [1085, 717]];
+    L.imageOverlay('assets/images/map2.svg', bounds).addTo(this.map);
+
+    /*     const icon = L.icon({
+          iconUrl: 'assets/images/marker.svg',
+          popupAnchor: [10, 10],
+          iconSize: [24, 40],
+          iconAnchor: [14, 40]
+        }); */
 
     const pandaIcon = L.icon({
       iconUrl: 'assets/images/panda-marker.svg',
       popupAnchor: [10, 10],
+      iconSize: [28, 46],
+      iconAnchor: [17, 46]
+    });
+
+    const rectangle = L.icon({
+      iconUrl: 'assets/images/rect.png',
+      popupAnchor: [-20, -30],
+      iconSize: [50, 50]
     });
 
     const buttonRemove =
       '<button type="button" class="remove">delete marker</button>';
 
-    const marker = L.marker([0, 0], { icon }).addTo(this.map).bindPopup('buttonRemove');
-    const marker2 = L.marker([100, 100], { icon }).addTo(this.map).bindPopup('buttonRemove');
-    const panda = L.marker([-100, -100], { icon: pandaIcon }).addTo(this.map).bindPopup('buttonRemove');
+    //const panda = L.marker([-5, 155], { icon: pandaIcon }).addTo(this.map).bindPopup('buttonRemove');
+
+
+    /*     const marker = L.marker([-5, 165], { icon }).addTo(this.map).bindPopup('buttonRemove');
+
+
+        const marker2 = L.marker([100, 100], { icon }).addTo(this.map).bindPopup('buttonRemove');
+        const marker3 = L.marker([200, 200], { icon }).addTo(this.map).bindPopup('buttonRemove'); */
+
+    const createPopup = (animal: string, country: string) => {
+      return `<div class="popup"><div class="popup__title">${animal},</div><div class="popup__subtitle">${country}</div><button class="popup__button">Watch online</button></div>`;
+    };
+
+    const pandaUrl = 'assets/images/panda-marker.svg',
+      pandaBounds = [[13, 140], [-7, 160]];
+    const panda = L.imageOverlay(pandaUrl, pandaBounds, { className: 'panda' }).addTo(this.map);
+    const pandaRect = L.marker([5, 150], { icon: rectangle }).addTo(this.map).bindPopup(createPopup('Panda', 'China'));
+
+
+    const pandasUrl = 'assets/images/pandas.svg',
+      pandasBounds = [[-20, 140], [-40, 160]];
+
+    const pandas = L.imageOverlay(pandasUrl, pandasBounds, { className: 'panda' }).addTo(this.map);
+    const pandasRect = L.marker([-30, 150], { icon: rectangle }).addTo(this.map).bindPopup(createPopup('Panda', 'China'));
 
 
 
-    const marker3 = L.marker([200, 200], { icon }).addTo(this.map).bindPopup('buttonRemove');
   }
 }
