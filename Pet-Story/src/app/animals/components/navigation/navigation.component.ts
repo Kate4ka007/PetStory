3ad99763f261029ebad2e3c08f10dec6d6524367
Kate4ka007 <shlemena@mail.ru>
2023-01-02 18:17:07 +0300

    feat: add icons and styles for animals navigation

--- Pet-Story/src/app/animals/components/navigation/navigation.component.ts
+++ Pet-Story/src/app/animals/components/navigation/navigation.component.ts
@@ -6,6 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  /*   links = [
+      { name: 'gorilla', path: 'assets/images/gorilla_icon.svg' },
+      { name: 'crocodile', path: 'assets/images/crocodile_icon.svg' },
+      { name: 'panda', path: 'assets/images/panda_icon.svg' },
+      { name: 'eagle', path: 'assets/images/eagle_icon.svg' },
+      { name: 'sloth', path: 'assets/images/sloth_icon.svg' },
+      { name: 'penguin', path: 'assets/images/penguin_icon.svg' },
+      { name: 'cheetah', path: 'assets/images/cheetah_icon.svg' },
+    ]; */
+
+  links = [
+    'gorilla',
+    'crocodile',
+    'panda',
+    'eagle',
+    'sloth',
+    'penguin',
+    'cheetah',
+  ];
 
   constructor() { }
 
