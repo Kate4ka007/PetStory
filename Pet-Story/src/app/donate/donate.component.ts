14bdb86b200e754b8b40835e9bd136c26ba64a28
Kate4ka007 <shlemena@mail.ru>
2023-01-07 17:30:49 +0300

    feat: add blocks for donate

--- Pet-Story/src/app/donate/donate.component.ts
+++ Pet-Story/src/app/donate/donate.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-donate',
   templateUrl: './donate.component.html',
-  styleUrls: ['./donate.component.scss']
+  styleUrls: ['./donate.component.scss'],
 })
 export class DonateComponent implements OnInit {
+  params = '';
 
-  constructor() { }
+  animal = '';
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      const id = params['id'];
+      this.params = `assets/images/${id}_donate.png`;
+      this.animal = id;
+      console.log(this.params);
+    });
   }
-
 }
